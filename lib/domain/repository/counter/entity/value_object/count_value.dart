7e4bce608270cf1b637329d11d452c45d4f45444
kolbe <water.northground@gmail.com>
2023-08-15 22:20:42 +0900

    change count lower limit

--- lib/domain/repository/counter/entity/value_object/count_value.dart
+++ lib/domain/repository/counter/entity/value_object/count_value.dart
@@ -12,7 +12,7 @@ class CountValue with _$CountValue {
   const CountValue._();
 
   static const int _upperLimit = 99;
-  static const int _lowerLimit = 99;
+  static const int _lowerLimit = 0;
 
   /// 上限に達すると上限値を返却する
   CountValue get increment => count == _upperLimit ? copyWith(count: _upperLimit) : copyWith(count: count + 1);
