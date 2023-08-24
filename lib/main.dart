1dc8f6dd36e81f0be04ed9d3996cdc1387c36791
kolbe <water.northground@gmail.com>
2023-08-24 23:49:33 +0900

    unused comment

--- lib/main.dart
+++ lib/main.dart
@@ -40,12 +40,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _counter = 0;
 
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
