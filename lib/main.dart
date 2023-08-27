e51b569b25dd5c2f30befba8ee75e3d93e355d25
kolbe <water.northground@gmail.com>
2023-08-27 16:55:36 +0900

    resolve analytics error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'inflastracture/firebase/counter/counter_repository.dart';
