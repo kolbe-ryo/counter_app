bb8ccab1ff0daf605a7eeca6c962502b3f557b5e
kolbe <water.northground@gmail.com>
2023-08-31 00:22:22 +0900

    define base app button

--- lib/presentation/common/app_button.dart
+++ lib/presentation/common/app_button.dart
@@ -3,14 +3,10 @@ import 'package:flutter/material.dart';
 class AppButton extends StatelessWidget {
   const AppButton({
     super.key,
-    required this.backgroundColor,
-    required this.buttonName,
-    required this.function,
+    required this.style,
   });
 
-  final Color backgroundColor;
-  final String buttonName;
-  final void Function() function;
+  final AppButtonStyle style;
 
   static const _buttonHeight = 60.0;
   static const _buttonWidthRate = 0.66;
@@ -20,22 +16,77 @@ class AppButton extends StatelessWidget {
     return SizedBox(
       height: _buttonHeight,
       width: MediaQuery.of(context).size.width * _buttonWidthRate,
-      child: ElevatedButton(
-        onPressed: function,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: backgroundColor,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(_buttonHeight / 2),
-            ),
-          ),
-          textStyle: const TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
+      child: style.generate(context),
+    );
+  }
+}
+
+// TODO: ストラテジーパターンで、アイコンボタンとテキストボタンを実装する
+abstract class AppButtonStyle {
+  Widget generate(BuildContext context);
+}
+
+class AppIconButton implements AppButtonStyle {
+  const AppIconButton({
+    required this.backgroundColor,
+    required this.buttonName,
+    required this.function,
+  });
+
+  final Color backgroundColor;
+  final String buttonName;
+  final void Function() function;
+
+  @override
+  Widget generate(BuildContext context) {
+    return ElevatedButton.icon(
+      icon: const Icon(Icons.account_box),
+      onPressed: function,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(AppButton._buttonHeight / 2),
           ),
         ),
-        child: Text(buttonName),
+        textStyle: const TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
       ),
+      label: Text(buttonName),
+    );
+  }
+}
+
+class AppNormalButton implements AppButtonStyle {
+  const AppNormalButton({
+    required this.backgroundColor,
+    required this.buttonName,
+    required this.function,
+  });
+
+  final Color backgroundColor;
+  final String buttonName;
+  final void Function() function;
+
+  @override
+  Widget generate(BuildContext context) {
+    return ElevatedButton(
+      onPressed: function,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(AppButton._buttonHeight / 2),
+          ),
+        ),
+        textStyle: const TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      child: Text(buttonName),
     );
   }
 }
