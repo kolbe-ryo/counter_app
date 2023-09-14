0762ef3a151458a933b8dc0309ec8f9980eb98ba
kolbe <water.northground@gmail.com>
2023-09-14 23:19:17 +0900

    change transition duration

--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -128,13 +128,14 @@ class EditPageRoute extends GoRouteData {
   Page<void> buildPage(BuildContext context, GoRouterState state) {
     return CustomTransitionPage<void>(
       child: const EditPage(),
+      transitionDuration: const Duration(milliseconds: 310),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
         return FadeTransition(
           opacity: animation,
           child: SlideTransition(
             position: animation.drive(
               Tween<Offset>(
-                begin: const Offset(0, 0.3),
+                begin: const Offset(0, 0.05),
                 end: Offset.zero,
               ).chain(
                 CurveTween(curve: Curves.easeInOut),
