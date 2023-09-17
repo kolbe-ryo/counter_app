dfaff411a2f451d279ff4584b99abcaeab0bcb86
kolbe <water.northground@gmail.com>
2023-09-17 19:54:49 +0900

    standardization

--- lib/presentation/pages/edit/edit_page.dart
+++ lib/presentation/pages/edit/edit_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../constant_value.dart';
-import '../main/main_page_icon_button.dart';
+import '../../../application/counter/state/edit_state.dart';
+import '../../common/counter_card.dart';
 import 'edit_page_header.dart';
 
 class EditPage extends StatelessWidget {
@@ -26,81 +26,14 @@ class EditPage extends StatelessWidget {
 class _EditCard extends ConsumerWidget {
   const _EditCard();
 
-  // TODO:  Cardを共通化する
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(editCardStateNotifierNotifierProvider);
     return SizedBox(
       height: 300,
       width: MediaQuery.of(context).size.width,
-      child: Card(
-        clipBehavior: Clip.none,
-        elevation: 1,
-        shape: RoundedRectangleBorder(
-          // BorderRadius.onlyからこちらに変更するとリストのレンダリングが爆速化する
-          borderRadius: BorderRadius.circular(20),
-        ),
-        color: Colors.amber,
-        child: InkWell(
-          borderRadius: BorderRadius.circular(20),
-          onTap: () {},
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Stack(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(top: kPadding / 2),
-                          child: Text(
-                            'test',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            MainPageIconButton.addCount(),
-                            MainPageIconButton.minusCount(),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        MainPageIconButton.edit(),
-                        MainPageIconButton.remove(),
-                      ],
-                    ),
-                  ],
-                ),
-                const Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: EdgeInsets.only(top: kPadding / 2),
-                    child: Text(
-                      '10',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+      child: CounterCard.forEdit(
+        counter: counter,
       ),
     );
   }
