aa1677cd37ca7aef41ab45b8cafdff53ab20ecfd
kolbe <water.northground@gmail.com>
2023-09-23 12:14:50 +0900

    outline for minimum layout

--- lib/presentation/pages/other/other_page.dart
+++ lib/presentation/pages/other/other_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../router.dart';
+import '../../../util/logger.dart';
+import '../../../util/text_styles.dart';
+import '../../common/buttons/back_app_button.dart';
+import '../../constant_value.dart';
+import 'menu_card.dart';
 
 class OtherPage extends ConsumerWidget {
   const OtherPage({super.key});
@@ -9,14 +13,48 @@ class OtherPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Menu'),
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () => ref.watch(routerProvider).go(MainPageRoute.name),
-          child: const Text('Other Page For Menu'),
+      appBar: AppBar(),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(kPadding),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                _AboutThisApp(),
+                _AboutThisApp(),
+              ],
+            ),
+            BackAppButton(),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _AboutThisApp extends StatelessWidget {
+  const _AboutThisApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 2,
+      child: MenuCard.square(
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.apple,
+              size: settingIconSize,
+            ),
+            SizedBox(height: kPadding),
+            Text(
+              'About Thid App',
+              style: TextStyles.largeFontStyle,
+            ),
+          ],
+        ),
+        onTap: () => logger.info('tap'),
       ),
     );
   }
