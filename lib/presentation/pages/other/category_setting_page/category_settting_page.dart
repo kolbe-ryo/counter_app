82195da93322cde923040065e66837f7e4107935
kolbe <water.northground@gmail.com>
2023-09-24 12:00:06 +0900

    category setting page #1

---
+++ lib/presentation/pages/other/category_setting_page/category_settting_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CategorySettingPage extends ConsumerStatefulWidget {
+  const CategorySettingPage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CategorySettingPageState();
+}
+
+class _CategorySettingPageState extends ConsumerState<CategorySettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
