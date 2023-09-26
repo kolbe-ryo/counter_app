5b1db69255843de0809c5afcb3ff2e559bd694cb
kolbe <water.northground@gmail.com>
2023-09-26 22:49:05 +0900

    add todo #6

--- lib/presentation/pages/main/strategy/main_page_icon_action.dart
+++ lib/presentation/pages/main/strategy/main_page_icon_action.dart
@@ -1,13 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
+import '../../../../application/counter/state/edit_state.dart';
+import '../../../../domain/repository/counter/entity/counter.dart';
 import '../../../../util/logger.dart';
 import '../../../router.dart';
+import '../../edit/edit_page.dart';
 import 'icon_interface.dart';
 
 class AddHeaderIconAction implements IconInterface {
   @override
-  void onTap(WidgetRef ref) {
-    logger.info('ADD Header');
+  Future<void> onTap(WidgetRef ref) async {
+    ref.read(editCardStateNotifierProvider.notifier).updateState(Counter.empty());
+    // TODO: 戻った時に登録するか決める
+    await showCupertinoModalBottomSheet<bool?>(
+      duration: const Duration(milliseconds: 200),
+      context: ref.context,
+      builder: (_) => const EditPage(),
+    );
   }
 }
 
