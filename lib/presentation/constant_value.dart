ece77b837c7725931db44692ba83769d0724f75b
kolbe <water.northground@gmail.com>
2023-09-30 17:00:40 +0900

    add colors #6

--- lib/presentation/constant_value.dart
+++ lib/presentation/constant_value.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 /// For Layout
 const kPadding = 16.0;
 
@@ -9,3 +11,43 @@ const settingIconSize = 40.0;
 const aboutAppUrl = 'https://kolbe-app.site/';
 
 const contactUrl = '${aboutAppUrl}contact/';
+
+/// Colors
+const appColors = [
+  Colors.pink,
+  Colors.pinkAccent,
+  Colors.red,
+  Colors.redAccent,
+  Colors.deepOrange,
+  Colors.deepOrangeAccent,
+  Colors.orange,
+  Colors.orangeAccent,
+  Colors.amber,
+  Colors.amberAccent,
+  Colors.yellow,
+  Colors.yellowAccent,
+  Colors.lime,
+  Colors.limeAccent,
+  Colors.lightGreen,
+  Colors.lightGreenAccent,
+  Colors.green,
+  Colors.greenAccent,
+  Colors.teal,
+  Colors.tealAccent,
+  Colors.cyan,
+  Colors.cyanAccent,
+  Colors.lightBlue,
+  Colors.lightBlueAccent,
+  Colors.blue,
+  Colors.blueAccent,
+  Colors.indigo,
+  Colors.indigoAccent,
+  Colors.purple,
+  Colors.purpleAccent,
+  Colors.deepPurple,
+  Colors.deepPurpleAccent,
+  Colors.blueGrey,
+  Colors.brown,
+  Colors.grey,
+  Colors.black,
+];
