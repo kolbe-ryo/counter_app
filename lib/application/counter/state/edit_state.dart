2186ba743cc58f018db69bf277b5769f859aeff6
kolbe <water.northground@gmail.com>
2023-09-30 15:22:57 +0900

    add abstract class for starategy pattern #6

--- lib/application/counter/state/edit_state.dart
+++ lib/application/counter/state/edit_state.dart
@@ -9,25 +9,36 @@ final editCardStateNotifierProvider = StateNotifierProvider<EditCardStateNotifie
   (ref) => EditCardStateNotifier(),
 );
 
-class EditCardStateNotifier extends StateNotifier<Counter> {
+abstract class EditCardStateInterface {
+  void updateState(Counter counter);
+  void changeCounterName(String name);
+  void changeCategory(CategoryInfo categoryInfo);
+  void changeDescription(String? description);
+}
+
+class EditCardStateNotifier extends StateNotifier<Counter> implements EditCardStateInterface {
   EditCardStateNotifier() : super(Counter.empty());
 
   // ignore: use_setters_to_change_properties
+  @override
   void updateState(Counter counter) {
     state = counter;
   }
 
   /// 名前を変更する
+  @override
   void changeCounterName(String name) {
     state = state.copyWith(name: StringValue(name));
   }
 
   /// カテゴリを変更する
+  @override
   void changeCategory(CategoryInfo categoryInfo) {
     state = state.copyWith(categoryInfo: categoryInfo);
   }
 
   /// 説明を変更する
+  @override
   void changeDescription(String? description) {
     state = state.copyWith(description: description);
   }
