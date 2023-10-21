33155966acd95d8379d8feae7fddce9155f983c3
kolbe <water.northground@gmail.com>
2023-10-21 22:43:14 +0900

    preparation for test #13

--- test/unit/button_unit_test.dart
+++ test/unit/button_unit_test.dart
@@ -1,2 +1,21 @@
-
 // TODO: Unitテストを記述すること
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('test name', () {
+    final counter = Counter();
+
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+}
+
+class Counter {
+  int value = 0;
+
+  void increment() => value++;
+
+  void decrement() => value--;
+}
